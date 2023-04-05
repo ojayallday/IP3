e3352a761d9e7a13645a695102d59e6284e6f0bc
Julius Ojala Ojakaa <ojayallday@gmail.com>
2023-04-06 00:15:48 +0300

    hovering over portfolio visual effects with jquery and css complete

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -28,4 +28,78 @@ $(document).ready(function(){
         $(".pmexplain").toggle();
         $(".pmimg").toggle();
     })
+
+    $("#portfolioimg1").hover(
+        function(){
+        $("#hover1").show();
+        },
+        function(){
+            $("#hover1").hide();
+        }
+    )
+
+    $("#portfolioimg2").hover(
+        function(){
+        $("#hover2").show();
+        },
+        function(){
+            $("#hover2").hide();
+        }
+    )
+
+    $("#portfolioimg3").hover(
+        function(){
+        $("#hover3").show();
+        },
+        function(){
+            $("#hover3").hide();
+        }
+    )
+
+    $("#portfolioimg4").hover(
+        function(){
+        $("#hover4").show();
+        },
+        function(){
+            $("#hover4").hide();
+        }
+    )
+
+    $("#portfolioimg5").hover(
+        function(){
+        $("#hover5").show();
+        },
+        function(){
+            $("#hover5").hide();
+        }
+    )
+
+    $("#portfolioimg6").hover(
+        function(){
+        $("#hover6").show();
+        },
+        function(){
+            $("#hover6").hide();
+        }
+    )
+
+    $("#portfolioimg7").hover(
+        function(){
+        $("#hover7").show();
+        },
+        function(){
+            $("#hover7").hide();
+        }
+    )
+
+    $("#portfolioimg8").hover(
+        function(){
+        $("#hover8").show();
+        },
+        function(){
+            $("#hover8").hide();
+        }
+    )
+
+    
 })
